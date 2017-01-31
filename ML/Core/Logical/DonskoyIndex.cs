9dc1a23558cc202f4e7eceeb1ff4786b06c8b3a5
Полянских Сергей Валерьевич <spmathf@gmail.com>
2017-01-31 17:51:40 +0300

    add missing files

---
+++ ML/Core/Logical/DonskoyIndex.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ML.Core.Logical
+{
+  /// <summary>
+  /// V.I. Donskoy Index
+  /// </summary>
+  public class DonskoyIndex : IndexBase
+  {
+    public override string ID { get {  return "DONS"; } }
+    public override string Name { get { return "Donskoy Index"; } }
+
+    public override double Calculate(Predicate<Point> pattern, ClassifiedSample sample)
+    {
+      int result = 0;
+      var prevs = new List<KeyValuePair<Point, Class>>();
+
+      var array = sample.ToArray();
+      var len = array.Length;
+
+      for (int i=0; i<len; i++)
+      for (int j=0; j<i; j++)
+      {
+        var ai = array[i];
+        var aj = array[j];
+
+        if (pattern(ai.Key) != pattern(aj.Key) && !ai.Value.Equals(aj.Value))
+            result++;
+      }
+
+      return result;
+    }
+  }
+}
---
+++ ML/Core/Logical/GiniIndex.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ML.Core.Logical
+{
+  /// <summary>
+  /// Gini index
+  /// </summary>
+  public class GiniIndex : IndexBase
+  {
+    public override string ID { get {  return "GINI"; } }
+    public override string Name { get { return "Gini Index"; } }
+
+    public override double Calculate(Predicate<Point> pattern, ClassifiedSample sample)
+    {
+      int result = 0;
+      var prevs = new List<KeyValuePair<Point, Class>>();
+
+      var array = sample.ToArray();
+      var len = array.Length;
+
+      for (int i=0; i<len; i++)
+      for (int j=0; j<i; j++)
+      {
+        var ai = array[i];
+        var aj = array[j];
+
+        if (pattern(ai.Key) == pattern(aj.Key) && ai.Value.Equals(aj.Value))
+            result++;
+      }
+
+      return result;
+    }
+  }
+}
